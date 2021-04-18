c491b0dacab3f12db1b9d2be393502b610d606be
Eirik Sletteberg <eiriksletteberg@gmail.com>
2021-04-18 22:04:04 +0200

    Use code from sb-init to test
    This will let us test functionality like the MDX integration,
    and simulate a successful sb-init bootstrap.

--- packages/example-react/.storybook/main.js
+++ packages/example-react/.storybook/main.js
@@ -1,7 +1,13 @@
 module.exports = {
-    stories: ['../stories/*'],
-    addons: ['@storybook/addon-essentials'],
-    core: {
-        builder: 'storybook-builder-vite',
-    },
-};
+    "stories": [
+        "../stories/**/*.stories.mdx",
+        "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    "addons": [
+        // "@storybook/addon-links",
+        "@storybook/addon-essentials"
+    ],
+    "core": {
+        "builder": "storybook-builder-vite"
+    }
+}
--- packages/example-react/stories/emoji-button.stories.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { EmojiButton } from './emoji-button';
-
-export default { component: EmojiButton, title: 'Examples / Emoji Button' };
-
-export const WithArgs = (args) => <EmojiButton {...args} />;
-WithArgs.args = { label: 'With args' };
-export const Basic = () => <EmojiButton label="Click me" />;
