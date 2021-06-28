09486e784ab53436c237d3736764b78d890f992a
Schalk Bower <schalk@hotmail.co.nz>
2021-06-29 07:53:45 +1000

    Sort stories by their export order

---
+++ packages/storybook-builder-vite/named-export-order-plugin.js
@@ -0,0 +1,32 @@
+const { parse } = require('es-module-lexer');
+
+module.exports.namedExportOrderPlugin = function namedExportOrderPlugin(options) {
+  return {
+    name: 'storybook-vite-named-export-order-plugin',
+    // This should only run after the typescript has been transpiled
+    enforce: 'post',
+    async transform(code, id) {
+      /**
+       * This doesn't feel right, we already get a list of stories in generateIframeScriptCode
+       * based on presets.apply('stories'). What's the best way for us to reuse those stories?
+       * Should we introduce a viteBuilderContext?
+       * 
+       * await options.presets.apply('stories')
+       */
+      if (!/\.stories\.(t|j)sx?$/.test(id)) {
+        return;
+      }
+
+      const [, exports] = parse(code);
+      if (exports.includes('__namedExportsOrder')) {
+        // user has defined named exports already
+        return null;
+      }
+
+      const orderedExports = exports.filter(e => e !== 'default');
+      const exportsArray = `['${orderedExports.join("', '")}']`;
+
+      return `${code};\nexport const __namedExportsOrder = ${exportsArray};`;
+    }
+  }
+}
