496312ed69e23ca98f7427af8642c9146b981f1a
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2021-11-18 01:12:07 +0000

    Support `features.storyStoreV7` (#126)
    Closes #125

--- packages/example-react/.storybook/main.js
+++ packages/example-react/.storybook/main.js
@@ -5,12 +5,15 @@ module.exports = {
         '../stories/**/*.stories.@(js|jsx|ts|tsx)',
     ],
     addons: [
-        // "@storybook/addon-a11y", // still breaks
+        "@storybook/addon-a11y",
         '@storybook/addon-links',
         '@storybook/addon-essentials',
     ],
     core: {
         builder: 'storybook-builder-vite',
+    },
+    features: {
+      storyStoreV7: true,
     },
     async viteFinal(config, { configType }) {
         // customize the Vite config here
--- packages/storybook-builder-vite/transform-iframe-html.js
+++ packages/storybook-builder-vite/transform-iframe-html.js
@@ -1,20 +1,24 @@
 module.exports.transformIframeHtml = async function transformIframeHtml(
     html,
-    { title, framework, presets }
+    { configType, features, framework, presets, serverChannelUrl, title }
 ) {
     const headHtmlSnippet = await presets.apply('previewHead');
     const bodyHtmlSnippet = await presets.apply('previewBody');
     const logLevel = await presets.apply('logLevel', undefined);
     const frameworkOptions = await presets.apply(`${framework}Options`, {});
-    const features = await presets.apply('features', {});
+    const coreOptions = await presets.apply('core');
+
     return html
         .replace('<!-- [TITLE HERE] -->', title || 'Storybook')
+        .replace('[CONFIG_TYPE HERE]', configType)
         .replace('[LOGLEVEL HERE]', logLevel)
         .replace(
             `'[FRAMEWORK_OPTIONS HERE]'`,
             JSON.stringify(frameworkOptions || {})
         )
+        .replace(`'[CHANNEL_OPTIONS HERE]'`, JSON.stringify(coreOptions && coreOptions.channelOptions ? coreOptions.channelOptions : {}))
         .replace(`'[FEATURES HERE]'`, JSON.stringify(features || {}))
+        .replace(`'[SERVER_CHANNEL_URL HERE]'`, JSON.stringify(serverChannelUrl))
         .replace('<!-- [HEAD HTML SNIPPET HERE] -->', headHtmlSnippet || '')
         .replace('<!-- [BODY HTML SNIPPET HERE] -->', bodyHtmlSnippet || '');
 };
