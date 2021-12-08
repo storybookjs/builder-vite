82703dbd82af1678f879bd96e7fee0fb5cb9de3e
kazuma1989 <kazuma.ebina@quipper.com>
2021-12-08 22:02:56 +0900

    fix: Handle StoriesEntry properly
    Use normalizeStories.

--- packages/builder-vite/list-stories.ts
+++ packages/builder-vite/list-stories.ts
@@ -1,20 +1,19 @@
 import * as path from 'path';
 import { promise as glob } from 'glob-promise';
+import { normalizeStories } from '@storybook/core-common';
 
-import type { Options, StoriesEntry } from '@storybook/core-common';
+import type { Options } from '@storybook/core-common';
 
-// TODO: Merge with https://github.com/storybookjs/builder-vite/pull/182
-export async function listStories({ presets, configDir }: Options) {
+export async function listStories(options: Options) {
   return (
     await Promise.all(
-      (
-        await presets.apply<Promise<StoriesEntry[]>>('stories')
-      ).map((storiesEntry) => {
-        const files = typeof storiesEntry === 'string' ? storiesEntry : storiesEntry.files;
-        if (!files) {
-          return [] as string[];
-        }
-        return glob(path.isAbsolute(files) ? files : path.join(configDir, files));
+      normalizeStories(await options.presets.apply('stories', [], options), {
+        configDir: options.configDir,
+        workingDir: options.configDir,
+      }).map(({ directory, files }) => {
+        const pattern = path.join(directory, files);
+
+        return glob(path.isAbsolute(pattern) ? pattern : path.join(options.configDir, pattern));
       })
     )
   ).reduce((carry, stories) => carry.concat(stories), []);
