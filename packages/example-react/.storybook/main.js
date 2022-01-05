cc2ba9eab2b2ed6fd379f168d034d8eb43e18571
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-01-04 22:39:05 -0500

    Remove storyStoreV7 flag from react example

--- packages/example-react/.storybook/main.js
+++ packages/example-react/.storybook/main.js
@@ -12,9 +12,6 @@ module.exports = {
     core: {
         builder: 'storybook-builder-vite',
     },
-    features: {
-      storyStoreV7: true,
-    },
     async viteFinal(config, { configType }) {
         // customize the Vite config here
         return config;
