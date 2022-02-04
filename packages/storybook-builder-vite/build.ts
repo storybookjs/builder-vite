409806c319ea0abba9a2701b19e75058862ecdba
Bodo Graumann <mail@bodograumann.de>
2022-02-04 16:46:43 +0100

    Refactor common vite config options in one place

--- packages/storybook-builder-vite/build.ts
+++ packages/storybook-builder-vite/build.ts
@@ -1,32 +1,21 @@
-import * as path from 'path';
 import { build as viteBuild } from 'vite';
-import { allowedEnvPrefix as envPrefix, stringifyProcessEnvs } from './envs';
-import { pluginConfig } from './vite-config';
+import { stringifyProcessEnvs } from './envs';
+import { commonConfig } from './vite-config';
 
-import type { UserConfig } from 'vite';
 import type { EnvsRaw, ExtendedOptions } from './types';
 
 export async function build(options: ExtendedOptions) {
   const { presets } = options;
 
+  const baseConfig = await commonConfig(options, 'development');
   const config = {
-    configFile: false,
-    root: path.resolve(options.configDir, '..'),
-    cacheDir: "node_modules/.vite-storybook",
+    ...baseConfig,
     build: {
       outDir: options.outputDir,
       emptyOutDir: false, // do not clean before running Vite build - Storybook has already added assets in there!
       sourcemap: true,
     },
-    envPrefix,
-    define: {},
-    resolve: {
-      alias: {
-        vue: 'vue/dist/vue.esm-bundler.js',
-      },
-    },
-    plugins: await pluginConfig(options, 'build'),
-  } as UserConfig;
+  };
 
   const finalConfig = await presets.apply('viteFinal', config, options);
 
