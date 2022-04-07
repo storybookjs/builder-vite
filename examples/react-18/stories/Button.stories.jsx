ae50c5b70ac75d854a3c5270bdcf2893976fdaea
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-04-06 22:59:44 -0400

    Add react 18 example

---
+++ examples/react-18/stories/Button.stories.jsx
@@ -0,0 +1,36 @@
+import { Button } from './Button';
+
+export default {
+  // no title, to demonstrate autotitle
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+export const Primary = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
+
+export const Secondary = {
+  args: {
+    label: 'Button',
+  },
+};
+
+export const Large = {
+  args: {
+    size: 'large',
+    label: 'Button',
+  },
+};
+
+export const Small = {
+  args: {
+    size: 'small',
+    label: 'Button',
+  },
+};
