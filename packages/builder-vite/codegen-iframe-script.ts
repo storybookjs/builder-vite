c0b6bdbdb213fce6f7d6c3dc199ea06f1c7e26c1
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-04-08 08:49:48 -0400

    Handle windows paths

--- packages/builder-vite/codegen-iframe-script.ts
+++ packages/builder-vite/codegen-iframe-script.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+import { normalizePath } from 'vite';
 import { virtualPreviewFile, virtualStoriesFile } from './virtual-file-names';
 
 import type { ExtendedOptions } from './types';
@@ -5,9 +7,10 @@ import type { ExtendedOptions } from './types';
 // We need to convert from an absolute path, to a traditional node module import path,
 // so that vite can correctly pre-bundle/optimize
 function transformPath(absPath: string) {
-  const splits = absPath.split('node_modules/');
+  const splits = absPath.split(`node_modules${path.sep}`);
   // Return everything after the final "node_modules/"
-  return splits[splits.length - 1];
+  const module = normalizePath(splits[splits.length - 1]);
+  return module;
 }
 
 export async function generateIframeScriptCode(options: ExtendedOptions) {
