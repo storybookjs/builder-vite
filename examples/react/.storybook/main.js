17abb004da363f14c8cf1d4c70763beedd9400c0
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-04-15 14:26:09 -0400

    Allow MDX stories ending with stories.mdx or story.mdx (#337)
    This aligns with a change that was made in storybook 6.2 to widen the naming allowed for mdx stories: https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#mdx-pattern-tweaked

--- examples/react/.storybook/main.js
+++ examples/react/.storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   framework: '@storybook/react',
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../stories/**/*stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-a11y', '@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
     builder: '@storybook/builder-vite',
--- packages/builder-vite/mdx-plugin.ts
+++ packages/builder-vite/mdx-plugin.ts
@@ -4,7 +4,7 @@ import { createCompiler } from '@storybook/csf-tools/mdx';
 /**
  * Storybook uses two different loaders when dealing with MDX:
  *
- * - *.stories.mdx are compiled with the CSF compiler
+ * - *stories.mdx and *story.mdx are compiled with the CSF compiler
  * - *.mdx are compiled with the MDX compiler directly
  *
  * @see https://github.com/storybookjs/storybook/blob/next/addons/docs/docs/recipes.md#csf-stories-with-arbitrary-mdx
@@ -13,7 +13,7 @@ export function mdxPlugin() {
   return mdx((filename) => {
     const compilers = [];
 
-    if (filename.includes('.stories.')) {
+    if (filename.endsWith('stories.mdx') || filename.endsWith('story.mdx')) {
       compilers.push(createCompiler({}));
     }
 
