1c79c4c253ddf4c8c2d235fdcc118203f3c5c451
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2022-04-22 10:52:30 -0400

    run prettier on lit-ts

--- examples/lit-ts/.storybook/main.ts
+++ examples/lit-ts/.storybook/main.ts
@@ -1,24 +1,16 @@
-const {mergeConfig} = require("vite");
-const postcssLit = require("rollup-plugin-postcss-lit");
+const { mergeConfig } = require('vite');
+const postcssLit = require('rollup-plugin-postcss-lit');
 
 module.exports = {
-  stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
     builder: '@storybook/builder-vite',
   },
-  framework: "@storybook/web-components",
+  framework: '@storybook/web-components',
   async viteFinal(config, { configType }) {
     return mergeConfig(config, {
-      plugins: [
-        postcssLit()
-      ]
-    })
+      plugins: [postcssLit()],
+    });
   },
-}
+};
