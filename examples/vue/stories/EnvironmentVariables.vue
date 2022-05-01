8eaec13e1a559fa708755b0e1eefec26f1b0bdf3
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-05-01 18:37:15 -0400

    Prevent vite:define from replacing in story source (#364)
    This fixes an issue where the vite:define plugin replaced environment variables inside the injected story source from `@storybook/source-loader`, which is supposed to be a string.  But when vite does the replacement, it adds its own quotes, which breaks the string.  
    
    For a simplified example, storybook might generate something like this:
    ```js
    var __STORY__ = "console.log(import.meta.env.VITE_VAR);"
    ```
    But then vite:define turns it into:
    ```js
    var __STORY__ = "console.log("env variable value");"
    ```
    
    Here's a reproduction from the user who originally reported this issue: https://github.com/victorlmneves/vite-storybook
    
    To test, I modified our environment variable stories.  In the process, I used some zero-width spaces to make the headings look a bit better, as suggested in https://vitejs.dev/guide/env-and-mode.html#production-replacement.
    
    I also decided to use `ts-node` instead of adding a babel config for the react-ts project.  At least until we figure out exactly how that .babelrc works.  
    
    
    * Add dynamic env var to story examples
    
    * Fix react-ts example
    
    * Prevent vite:define from replacing in story source
    
    * Clean up some types

--- examples/vue/stories/EnvironmentVariables.vue
+++ examples/vue/stories/EnvironmentVariables.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-    <h1>import . meta . env:</h1>
+    <h1>import​.meta​.env:</h1>
     <div>{{ env }}</div>
-    <h1>import . meta . env . STORYBOOK:</h1>
+    <h1>import​.meta​.env​.STORYBOOK:</h1>
     <div>{{ storybook }}</div>
+    <h1>dynamic:</h1>
+    <div>{{ dynamic }}</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'env-vars',
+  props: {
+    dynamic: {
+      type: String,
+      required: true,
+    },
+  },
   computed: {
     env() {
       return JSON.stringify(import.meta.env, null, 2);
