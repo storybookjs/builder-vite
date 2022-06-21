677f6b3e69e046f59dfc76dbe7763b20bb0cb3b7
赵東澔 <zhaodonghao586@outlook.com>
2022-06-21 12:10:00 +0800

    Configure for bb

--- examples/overview/.storybook/main.ts
+++ examples/overview/.storybook/main.ts
@@ -1,35 +1,10 @@
-import path from 'path';
-import { getPackagesInfo, OVERVIEW_BASE, withOverview } from '../../../scripts/build-utils';
-import pkg from '../package.json';
-
-const refs = getPackagesInfo()
-  // @ts-ignore
-  .filter((item) => !!item.packageJson.pages && item.packageJson.name !== pkg.name)
-  .reduce<
-    Record<
-      string,
-      {
-        title: string;
-        url: string;
-      }
-    >
-  >((prev, current) => {
-    const baseName = current?.path?.split('/').pop();
-    if (!baseName) {
-      return prev;
-    }
-    prev[baseName] = {
-      title: baseName,
-      url: path.join(OVERVIEW_BASE, baseName),
-    };
-    return prev;
-  }, {});
+import { getRefs, withOverview } from 'bookcase-builder';
 
 export default withOverview(__dirname)({
   stories: ['../stories/**/*stories.mdx'],
   addons: ['@storybook/addon-essentials'],
-  refs,
+  refs: getRefs(),
   core: {
     builder: '@storybook/builder-vite',
   }
-} as any);
+});
