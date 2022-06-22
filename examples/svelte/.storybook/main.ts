11c9f08c4ebcfb3abc458045a1cb4c6fb43d525d
赵東澔 <zhaodonghao586@outlook.com>
2022-06-22 13:30:52 +0800

    chore: Run prettier.

--- examples/svelte/.storybook/main.ts
+++ examples/svelte/.storybook/main.ts
@@ -1,5 +1,5 @@
 import { withOverview } from 'bookcase-builder';
-import preprocess from 'svelte-preprocess'
+import preprocess from 'svelte-preprocess';
 
 export default withOverview(__dirname)({
   framework: '@storybook/svelte',
--- examples/vue/.storybook/main.ts
+++ examples/vue/.storybook/main.ts
@@ -11,4 +11,4 @@ export default {
     // customize the Vite config here
     return config;
   },
-}
+};
