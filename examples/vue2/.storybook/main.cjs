3c3265dae5d5af6fede29ed9356fcbdd79b24a31
Keith Charles <keith.k.charles@gmail.com>
2022-07-29 13:02:26 -0600

    chore: add the correct packages and language
    change language of duped example to reflect vue2 usage.
    
    # Conflicts:
    #	examples/vue/yarn.lock
    #	examples/vue2/.storybook/main.cjs
    #	package.json

--- examples/vue2/.storybook/main.cjs
+++ examples/vue2/.storybook/main.cjs
@@ -2,7 +2,7 @@ const { resolve } = require('path');
 const { mergeConfig } = require('vite');
 
 module.exports = {
-  framework: '@storybook/vue3',
+  framework: '@storybook/vue',
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
   core: {
