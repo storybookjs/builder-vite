5b6bbab8511bc2781f82fdc4f6bb139f39431eea
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-09-23 15:04:59 -0400

    Make react example its own project

--- examples/react/.storybook/main.cjs
+++ examples/react/.storybook/main.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   framework: '@storybook/react',
   stories: ['../stories/**/*stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-a11y', '@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-a11y', '@storybook/addon-essentials'],
   core: {
     builder: '@storybook/builder-vite',
     // we don't want to muck up the data when we're working on the builder
